89552878214df82956d7bd01f7a296b0dca773bb
jihostudy <cale.jiho@gmail.com>
2025-03-11 17:21:00 +0900

    9. 스코프 미니퀘스트

---
+++ 9_스코프/miniquest.js
@@ -0,0 +1,17 @@
+/** 1번 퀘스트 */
+
+let temperature = 25;
+temperature = 30;
+
+const MAX_TEMPERATURE = 35;
+
+/**
+ MAX_TEMPERATURE에 다른 값을 할당하려 시도했을 때 발생하는 에러
+ const로 정의된 MAX_TEMPERATURE는 상수값으로 할당되어서 값을 재할당할 수 없습니다.
+ */
+
+/** 2번 퀘스트 */
+/**
+  isRaining은 let으로 선언하면 블록 스코프로 설정되기 때문에 if문 외부에서 접근하려고 하면
+  참조할 수 없는 값에 접근하는 것이기 때문에 에러가 발생합니다.
+ */
